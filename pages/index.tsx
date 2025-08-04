617edce50eb290864fac9b11c75ce1d2005c2b70
Tourtou <tourtouttx@proton.me>
2025-08-04 12:23:20 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,15 @@
-import React from "react";
+import Link from 'next/link';
 
 export default function Home() {
-  return <h1>Bienvenue sur AgoraPro</h1>;
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Bienvenue sur AgoraPro</h1>
+      <ul style={{ marginTop: '1rem' }}>
+        <li><Link href="/login">Se connecter</Link></li>
+        <li><Link href="/admin">Espace Admin</Link></li>
+        <li><Link href="/nouveau-debat">Créer un nouveau débat</Link></li>
+        <li><Link href="/profil">Voir le profil</Link></li>
+      </ul>
+    </div>
+  );
 }
